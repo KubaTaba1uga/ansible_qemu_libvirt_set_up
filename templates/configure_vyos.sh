5c1af7d023a151927f3ef8d589c89d75df1faf1c
KubaTaba1uga <jakub@buczynski.net.pl>
2023-12-29 22:27:55 +0100

    save working configuration via pexpect

--- templates/configure_vyos.sh
+++ templates/configure_vyos.sh
@@ -1,9 +1,20 @@
-#!/usr/bin/env vbash
+#!/usr/bin/env bash
 
-source /opt/vyatta/etc/functions/script-template
+expect <<EOF
 
-configure
-set interfaces ethernet eth1 address '172.16.0.2/16'
-commit
-save
+  set timeout 120
+  spawn virsh console {{ vyos_vm_name }} --force
 
+  expect "Escape character is"  {send "\r"}
+  expect "vyos login:"  {send "vyos\r"}
+  expect "Password:"  {send "vyos\r"}
+  expect "vyos@vyos:" {send "configure\r"}
+  expect "vyos@vyos*" {send "set interfaces ethernet eth0 address {{ router_public_ip }} \r"}
+  expect "vyos@vyos*" {send "set system name-server 1.1.1.1 \r"}
+  expect "vyos@vyos*" {send "set system host-name {{ vyos_vm_name }} \r"}
+  expect "vyos@vyos*" {send "commit \r"}
+  expect "vyos@vyos*" {send "save \r"}
+  expect "vyos@vyos*" {send "exit\r"}
+  expect "vyos@vyos*" {send "exit\r"}
+
+EOF
--- templates/startup_vyos.py
+++
@@ -1,9 +0,0 @@
-import os
-from pathlib import Path
-
-USER_CONFIG = Path("/dev/vdb")
-
-if not USER_CONFIG.exists():
-    exit(0)
-
-os.system(f"vbash {USER_CONFIG}")
