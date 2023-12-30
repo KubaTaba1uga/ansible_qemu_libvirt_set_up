4925051a36c69d7a679019c69395155203f6558a
KubaTaba1uga <jakub@buczynski.net.pl>
2023-12-30 23:17:04 +0100

    save working nat

--- templates/configure_vyos.sh
+++ templates/configure_vyos.sh
@@ -14,18 +14,26 @@ expect <<EOF
   expect "vyos@vyos*" {send "set system name-server '1.1.1.1'\r"}
   # Configure public interface
   expect "vyos@vyos*" {send "set interfaces ethernet eth0 address '{{ router_public_ip }}/{{ ip_cidr_netmask }}'\r"}
+  expect "vyos@vyos*" {send "set protocols static route 0.0.0.0/0 next-hop '{{ public_bridge_ip }}'\r"}
+  expect "vyos@vyos*" {send "set nat source rule 100 outbound-interface eth0\r"}
+  expect "vyos@vyos*" {send "set nat source rule 100 source address '{{ private_network_ip }}/{{ ip_cidr_netmask }}'\r"}
+  expect "vyos@vyos*" {send "set nat source rule 100 translation address '{{ router_public_ip }}'\r"}
   # Configure private interface
   expect "vyos@vyos*" {send "set interfaces ethernet eth1 address '{{ router_private_ip }}/{{ ip_cidr_netmask }}'\r"}
-  expect "vyos@vyos*" {send "set service dhcp-server shared-network-name PRIVATE subnet '{{ private_bridge_ip }}/{{ ip_cidr_netmask }}' default-router '{{ router_private_ip }}'\r"}
-  expect "vyos@vyos*" {send "set service dhcp-server shared-network-name PRIVATE subnet '{{ private_bridge_ip }}/{{ ip_cidr_netmask }}' domain-name 'private'\r"}
-  expect "vyos@vyos*" {send "set service dhcp-server shared-network-name PRIVATE subnet '{{ private_bridge_ip }}/{{ ip_cidr_netmask }}' lease '86400'\r"}
-  expect "vyos@vyos*" {send "set service dhcp-server shared-network-name PRIVATE subnet '{{ private_bridge_ip }}/{{ ip_cidr_netmask }}' range 100 start '{{ router_private_ip }}'\r"}
-  expect "vyos@vyos*" {send "set service dhcp-server shared-network-name PRIVATE subnet '{{ private_bridge_ip }}/{{ ip_cidr_netmask }}' exclude '{{ router_private_ip }}'\r"}
+  expect "vyos@vyos*" {send "set service dhcp-server shared-network-name PRIVATE subnet '{{ private_network_ip }}/{{ ip_cidr_netmask }}' default-router '{{ router_private_ip }}'\r"}
+  expect "vyos@vyos*" {send "set service dhcp-server shared-network-name PRIVATE subnet '{{ private_network_ip }}/{{ ip_cidr_netmask }}' domain-name 'private'\r"}
+  expect "vyos@vyos*" {send "set service dhcp-server shared-network-name PRIVATE subnet '{{ private_network_ip }}/{{ ip_cidr_netmask }}' lease '86400'\r"}
+  expect "vyos@vyos*" {send "set service dhcp-server shared-network-name PRIVATE subnet '{{ private_network_ip }}/{{ ip_cidr_netmask }}' range 0 start '{{ private_network_ip }}'\r"}
+  expect "vyos@vyos*" {send "set service dhcp-server shared-network-name PRIVATE subnet '{{ private_network_ip }}/{{ ip_cidr_netmask }}' range 0 stop '{{- private_network_ip | regex_replace('\d+$', '254') -}}'\r"}
+  expect "vyos@vyos*" {send "set service dhcp-server shared-network-name PRIVATE subnet '{{ private_network_ip }}/{{ ip_cidr_netmask }}' exclude '{{ router_private_ip }}'\r"}
+  expect "vyos@vyos*" {send "set service dhcp-server shared-network-name PRIVATE subnet '{{ private_network_ip }}/{{ ip_cidr_netmask }}' exclude '{{ private_bridge_ip }}'\r"}
+  expect "vyos@vyos*" {send "set service dns forwarding system\r"}
   expect "vyos@vyos*" {send "set service dns forwarding cache-size '0'\r"}
   expect "vyos@vyos*" {send "set service dns forwarding listen-address '{{ router_private_ip }}'\r"}
-  expect "vyos@vyos*" {send "set service dns forwarding allow-from '{{ private_bridge_ip }}/{{ ip_cidr_netmask }}'\r"}
-  expect "vyos@vyos*" {send "set service ssh \r"}
-  expect "vyos@vyos*" {send "set service ssh listen-address '{{ router_private_ip }}:22'\r"}
+  expect "vyos@vyos*" {send "set service dns forwarding allow-from '{{ private_network_ip }}/{{ ip_cidr_netmask }}'\r"}
+  expect "vyos@vyos*" {send "set service dns forwarding dhcp 'eth1'\r"}
+  expect "vyos@vyos*" {send "set service ssh port 22\r"}
+  expect "vyos@vyos*" {send "set service ssh listen-address '{{ router_private_ip }}'\r"}
   # Outro
   expect "vyos@vyos*" {send "commit\r"}
   expect "vyos@vyos*" {send "save\r"}
