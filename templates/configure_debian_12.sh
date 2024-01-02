9ff3f79bfdcc3507ee9d06f82192dc0c9b28cd5e
KubaTaba1uga <jakub@buczynski.net.pl>
2024-01-02 14:43:06 +0100

    fix bugs

--- templates/configure_debian_12.sh
+++ templates/configure_debian_12.sh
@@ -1,35 +1,61 @@
 #!/usr/bin/env bash
 
-ssh_key_file="/home/$SUDO_USER/.ssh/id_rsa.pub"
+# fix bug with missing hostname keys
+dpkg-reconfigure openssh-server
 
-if [ ! -f "$ssh_key_file" ]; then
-    mkdir -p /home/$SUDO_USER/.ssh
-    ssh-keygen -t rsa -f /home/$SUDO_USER/.ssh/id_rsa -N '' -P ''
-    chown $SUDO_USER -R /home/$SUDO_USER/.ssh/
-fi
+systemctl restart sshd
 
-ssh_key=$(cat $ssh_key_file)
 
-expect <<EOF
+# fix networking bug
+tee /network/interfaces <<EOF
+## This file describes the network interfaces available on your system
+## and how to activate them. For more information, see interfaces(5).
 
-  set timeout 300
-  spawn virsh console {{ vm_name }} --force
+source /etc/network/interfaces.d/*
 
-  expect "Escape character is"  {send "\r"}
-  expect "localhost login:"  {send "root\r"}
-  # Fix not working sshd 
-  expect "root@localhost*" {send "cd /etc/ssh/ ; ssh-keygen -A\r"}
-  # Add $SUDO_USER keys to VM's authorized keys
-  expect "root@localhost*" {send "mkdir -p /root/.ssh && echo $ssh_key >> /root/.ssh/authorized_keys\r"}
-  # Change hostname
-  expect "root@localhost*" {send "echo {{ vm_name }} > /etc/hostname \r"}
-  # Grow disk
-  expect "root@localhost*" {send "growpart /dev/vda 1\r"}
-  expect "root@localhost*" {send "resize2fs /dev/vda1\r"}
-  # Outro - DON'T TOUCH
-  expect "root@localhost*" {send "\r"}
-  expect "root@localhost*" {send "reboot now\r"}
-  expect "root@localhost*" {send "\r"}
+## The loopback network interface
+auto lo
+iface lo inet loopback
 
+## The primary network interface
+allow-hotplug enp1s0
+iface enp1s0 inet dhcp
 EOF
+
+systemctl restart networking
+
+
+
+# ssh_key_file="/home/$SUDO_USER/.ssh/id_rsa.pub"
+
+# if [ ! -f "$ssh_key_file" ]; then
+#     mkdir -p /home/$SUDO_USER/.ssh
+#     ssh-keygen -t rsa -f /home/$SUDO_USER/.ssh/id_rsa -N '' -P ''
+#     chown $SUDO_USER -R /home/$SUDO_USER/.ssh/
+# fi
+
+# ssh_key=$(cat $ssh_key_file)
+
+# expect <<EOF
+
+#   set timeout 300
+#   spawn virsh console {{ vm_name }} --force
+
+#   expect "Escape character is"  {send "\r"}
+#   expect "localhost login:"  {send "root\r"}
+#   # Fix not working sshd 
+#   expect "root@localhost*" {send "cd /etc/ssh/ ; ssh-keygen -A\r"}
+#   # Add $SUDO_USER keys to VM's authorized keys
+#   expect "root@localhost*" {send "mkdir -p /root/.ssh && echo $ssh_key >> /root/.ssh/authorized_keys\r"}
+#   # Change hostname
+#   expect "root@localhost*" {send "echo {{ vm_name }} > /etc/hostname \r"}
+#   # Grow disk
+#   expect "root@localhost*" {send "growpart /dev/vda 1\r"}
+#   expect "root@localhost*" {send "resize2fs /dev/vda1\r"}
+#   # Outro - DON'T TOUCH
+#   expect "root@localhost*" {send "\r"}
+#   expect "root@localhost*" {send "reboot now\r"}
+#   expect "root@localhost*" {send "\r"}
+
+# EOF
 
